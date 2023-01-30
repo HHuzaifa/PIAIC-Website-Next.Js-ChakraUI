b5ce26d6fb8f1c02159d6194e5c33456379a8a06
HHuzaifa <huzaifa50100@gmail.com>
2023-01-31 02:22:16 +0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import './globals.css'
+"use client"
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import Footer from "@/components/footer";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>
+          {children}
+          <Footer />
+        </ChakraProvider>
+      </body>
     </html>
-  )
+  );
 }
